4b48d36df212e3ff522a41ca177ec152c2ccee2e
Luigi Galbusera <galbu.luigi@gmail.com>
2026-03-09 16:48:48 +0100

    hot fix and some style

--- gutenberg-blocks/products-carousel/build/products-carousel/index.asset.php
+++ gutenberg-blocks/products-carousel/build/products-carousel/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '73fb90de0d489b694713');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9f5bdb80515b49a6732e');
